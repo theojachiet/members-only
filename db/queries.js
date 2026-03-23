20aa6de98a2e9a8290344cf3acf0b7f6919efef4
Theo <tjachiet@gmail.com>
2026-03-23 09:14:12 +0100

    add query function for creating a message

--- db/queries.js
+++ db/queries.js
@@ -16,8 +16,17 @@ async function removeAdmin(id) {
     await pool.query('UPDATE users SET is_admin = FALSE WHERE id = $1', [id]);
 }
 
+async function createMessage(user_id, text, date) {
+    await pool.query('INSERT INTO messages (user_id, text, date) VALUES ($1, $2, $3)', [
+        user_id,
+        text,
+        date
+    ])
+}
+
 module.exports = {
     addUser,
     makeAdmin,
     removeAdmin,
+    createMessage
 };
