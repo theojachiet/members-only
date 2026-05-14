c0e80b6019d305dc935eca58f91e827adc498c5f
Theo <tjachiet@gmail.com>
2026-05-14 17:44:59 +0200

    add connection string and .env file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
 
 const pool = new Pool({
-  connectionString: `postgresql://${process.env.DB_LOGIN}:${process.env.DB_PWD}@localhost:5432/${process.env.DB_NAME}`
+  connectionString: `${process.env.DATABASE_URL}`
 });
 
 const app = express();
