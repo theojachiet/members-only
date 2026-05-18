e0dd8b04d99c8eee31a3c6817951b577f543b6da
Theo <tjachiet@gmail.com>
2026-05-18 10:27:02 +0200

    clean post function and test while compiling .env file

--- app.js
+++ app.js
@@ -20,6 +20,19 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => res.render("index"));
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+      req.body.username,
+      req.body.password,
+    ]);
+    res.redirect("/");
+  } catch(err) {
+    return next(err);
+  }
+});
+
+
 app.listen(3000, (error) => {
   if (error) {
     throw error;
