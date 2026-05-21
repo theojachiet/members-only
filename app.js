93a12d71f95b968c8e2265e495e8de996e002a99
Theo <tjachiet@gmail.com>
2026-05-21 17:59:40 +0200

    add is_admin property false when creating a user

--- app.js
+++ app.js
@@ -86,9 +86,10 @@ app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async (req, res, next) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+    await pool.query("INSERT INTO users (username, password, is-admin) VALUES ($1, $2, $3)", [
       req.body.username,
       hashedPassword,
+      false
     ]);
     res.redirect("/");
   } catch (err) {
